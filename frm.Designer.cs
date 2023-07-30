6c0801c7cf4bd952e31fffe1bdfeb877bb712182
Dark <turkmvc@gmail.com>
2023-07-30 17:05:48 +0300

    first upload

---
+++ frm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Donguler
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progres = new ProgressBar();
+            btPositive = new Button();
+            btNegative = new Button();
+            SuspendLayout();
+            // 
+            // progres
+            // 
+            progres.Location = new Point(12, 30);
+            progres.Name = "progres";
+            progres.Size = new Size(762, 99);
+            progres.TabIndex = 0;
+            // 
+            // btPositive
+            // 
+            btPositive.Location = new Point(12, 151);
+            btPositive.Name = "btPositive";
+            btPositive.Size = new Size(191, 111);
+            btPositive.TabIndex = 1;
+            btPositive.Text = "+";
+            btPositive.UseVisualStyleBackColor = true;
+            btPositive.Click += btPositive_Click;
+            // 
+            // btNegative
+            // 
+            btNegative.Location = new Point(583, 151);
+            btNegative.Name = "btNegative";
+            btNegative.Size = new Size(191, 111);
+            btNegative.TabIndex = 1;
+            btNegative.Text = "-";
+            btNegative.UseVisualStyleBackColor = true;
+            btNegative.Click += btNegative_Click;
+            // 
+            // frm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 289);
+            Controls.Add(btNegative);
+            Controls.Add(btPositive);
+            Controls.Add(progres);
+            Name = "frm";
+            Text = "frm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ProgressBar progres;
+        private Button btPositive;
+        private Button btNegative;
+    }
+}
---
+++ frm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Donguler
+{
+    public partial class frm : Form
+    {
+        public frm()
+        {
+            InitializeComponent();
+        }
+        int pbar = 0;
+
+        private void btPositive_Click(object sender, EventArgs e)
+        {
+            if (progres.Value != 100)
+            {
+                progres.Value = pbar + 10;
+                pbar = progres.Value;
+            }
+            else
+            {
+                MessageBox.Show("Zaten 100");
+            }
+        }
+
+        private void btNegative_Click(object sender, EventArgs e)
+        {
+            if (progres.Value != 0)
+            {
+                progres.Value = pbar - 10;
+                pbar = progres.Value;
+            }
+            else
+            {
+                MessageBox.Show("Zaten 0");
+            }
+        }
+    }
+}
